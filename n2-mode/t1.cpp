7a49c4a99f6197cefc1913815c1b376ac842fbbf
patcharapongk-arch <6230362521@student.chula.ac.th>
2024-06-10 08:36:15 +0700

    WIP: Question2, recursive part

--- n2-mode/t1.cpp
+++ n2-mode/t1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 typedef vector<int> Vector;
@@ -32,9 +33,11 @@ Pair find_mode(int left, int right) {
   return {maxEntry->first, maxEntry->second};
 }
 
-Pair move_mid(int half) {
+Pair move_mid(int left, int half, int right) {
   int mid_entry = v[half];
   int half1, half2 = half;
+  if (v[left] == v[half]) {}
+  if (v[half] == v[right]) {}
   while (v[half2] == mid_entry) {
     ++half2;
   }
@@ -45,21 +48,27 @@ Pair move_mid(int half) {
 }
 
 
-int solve() {
-  int left = 0;
-  int half = v.size() / 2;
-  int right = v.size() - 1;
+Pair solve(int left, int right) {
+  if (v.size() == 0) return {-1, -1};
+  if (left == right) return {v[left], 1};
+  int half = left + (right-left) / 2;
   
-  int half1 = move_mid(half).first;
-  int half2 = move_mid(half).second;
+  Pair halves = move_mid(half);
+  int half1 = halves.first;
+  int half2 = halves.second;
 
-  Pair p1 = find_mode(left, half1);
-  Pair p2 = find_mode(half1, half2);
-  Pair p3 = find_mode(half2, right);
-  return max3(p1.second,p2.second,p3.second); 
+  Pair p1 = solve(left, half1);
+  Pair p2 = solve(half1, half2);
+  Pair p3 = solve(half2, right);
+
+  int max_count = max3(p1.second, p2.second, p3.second);
+  if (max_count == p1.second) return {p1.first, p1.second};
+  if (max_count == p2.second) return {p2.first, p2.second};
+  return {p3.first, p3.second};
 }
+ 
 
 
 int main() {
-  solve();
+  solve(0, v.size() - 1);
 }
