0ab575744ae5e2bc6d31775058b406996ecd6d5e
patcharapongk-arch <6230362521@student.chula.ac.th>
2024-06-10 08:36:04 +0700

    Question1 Done

--- n2-mode/t0.cpp
+++ n2-mode/t0.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <vector>
@@ -7,11 +8,11 @@ typedef vector<int> Vector;
 typedef map<int, int> Map;
 typedef pair<int, int> Pair;
 
-int max3(int a, int b, int c) {
-  return max(a, max(b,c));
-}
+int max3(int a, int b, int c) { return max(a, max(b, c)); }
 
-Vector v = {1, 1, 2, 2, 2, 2,3, 3, 3, 3, 3};
+Vector v = 
+{-2,-2,-1,-1,-1,0,2,}
+;
 
 
 // inclusive
@@ -23,34 +24,41 @@ Pair find_mode(int left, int right) {
 
   auto maxEntry = std::max_element(
       map.begin(), map.end(),
-      [](const Pair& a, const Pair& b) { return a.second < b.second; });
+      [](const Pair &a, const Pair &b) { return a.second < b.second; });
 
   return {maxEntry->first, maxEntry->second};
 }
 
-Pair solve(int left,int right) {
-  if (left == right) return {v[left], 1};
-  int half = (left + right) / 2;
+Pair solve(int left, int right) {
+  if (v.size() == 0) return {-1, -1};
+  if (left == right)
+    return {v[left], 1};
+  
+  int half = left + (right - left) / 2;
 
   Pair p1 = solve(left, half);
-  Pair p2 = solve(half+1, right);
-  
+  Pair p2 = solve(half + 1, right);
+
   int mid_mode = v[half];
   int mid_mode_count = 1;
-  if (v[half] == v[half+1]) {
+  if (half + 1 < right && v[half] == v[half + 1]) {
     int h1 = half;
-    int h2 = half+1;
-    while (h1 >= 0 && v[h1] == v[half]) --h1;
-    while (h2 < v.size() && v[h2] == v[half]) ++h2;
-    mid_mode_count = h2-h1-1;
+    int h2 = half + 1;
+    while (h1 >= 0 && v[h1] == v[half])
+      --h1;
+    while (h2 < v.size() && v[h2] == v[half])
+      ++h2;
+    mid_mode_count = h2 - h1 - 1;
   }
   int max_count = max3(p1.second, p2.second, mid_mode_count);
-  if (max_count == p1.second) return {p1.first, p1.second};
-  if (max_count == p2.second) return {p2.first, p2.second};
-return {mid_mode, mid_mode_count};
+  if (max_count == p1.second)
+    return {p1.first, p1.second};
+  if (max_count == p2.second)
+    return {p2.first, p2.second};
+  return {mid_mode, mid_mode_count};
 }
 
 int main() {
-  Pair p = solve(0, v.size()-1);
+  Pair p = solve(0, v.size() - 1);
   printf("val %d count %d\n", p.first, p.second);
 }
